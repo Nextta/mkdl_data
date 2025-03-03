6a05ef3911fe47554bf49ec6b00ad94a590faac2
JairoDev <jairobernaldev@gmail.com>
2025-03-03 11:28:03 +0100

    Añadido EODHD y YFinance

--- mkdl_data.py
+++ mkdl_data.py
@@ -2,11 +2,13 @@ import os
 from dotenv import load_dotenv
 import requests
 import pandas as pd
+import yfinance as yf
 
 class mkdl_data:
     def __init__(self):
         load_dotenv()
         self.marketstack_api_key = os.environ.get('marketstack_api_key')
+        self.eodhd_api_key = os.environ.get('key_EODHD')
 
     def get_data_marketstack(self, symbol=None, date_from=None, date_to=None, timeframe=None, sort="ASC"):
         """
@@ -36,13 +38,71 @@ class mkdl_data:
 
             response = requests.get(url)
 
-        if response.status_code == 200:
-            data = response.json()
+            if response.status_code == 200:
+                data = response.json()
 
-            if data['data']:
-                df = pd.DataFrame(data['data'])
+                if data['data']:
+                    df = pd.DataFrame(data['data'])
+                    df.index = pd.to_datetime(df['date'])
+                    df = df.drop(columns=['date'])
+                    return df
+            else:
+                print("Error: No se optuvo respuesta del servidor")
+    
+    def get_data_eodhd(self, symbol=None, date_from=None, date_to=None, timeframe="d"):
+        """
+            Obtiene datos históricos de fin de día para un símbolo dado desde la API de EODHD.
+            Parámetros:
+            symbol (str): El símbolo de la acción para obtener datos. Por defecto es None.
+            date_from (str): La fecha de inicio para obtener datos en formato 'YYYY-MM-DD'. Por defecto es None.
+            date_to (str): La fecha de fin para obtener datos en formato 'YYYY-MM-DD'. Por defecto es None.
+            timeframe (str): El intervalo de tiempo para los datos. Por defecto es "d" (diario).
+            Retorna:
+            pd.DataFrame: Un DataFrame que contiene los datos históricos con la fecha como índice.
+            Lanza:
+            requests.exceptions.RequestException: Si hay un problema con la solicitud de red.
+            ValueError: Si el código de estado de la respuesta no es 200.
+        """
+
+        if symbol is not None:
+            url = f"https://eodhd.com/api/eod/{symbol}?api_token={self.eodhd_api_key}&fmt=json&period={timeframe}"
+
+            if date_from is not None:
+                url += f"&from={date_from}"
+            
+            if date_to is not None:
+                url += f"&to={date_to}"
+
+            response = requests.get(url)
+
+            if response.status_code == 200:
+                data = response.json()
+                df = pd.DataFrame(data)
                 df.index = pd.to_datetime(df['date'])
                 df = df.drop(columns=['date'])
                 return df
-        else:
-            print("Error: No se optuvo respuesta del servidor")
+    
+    def get_data_yfinance(self, symbol=None, date_from=None, date_to=None, timeframe=None):
+
+        if timeframe is not None and symbol is not None:
+            if date_from is not None and date_to is not None:
+                df = yf.download(symbol, start=date_from, end=date_to, interval=timeframe)
+            elif date_from is not None:
+                df = yf.download(symbol, start=date_from, interval=timeframe)
+            elif date_to is not None:
+                df = yf.download(symbol, end=date_to, interval=timeframe)
+            else:
+                df = yf.download(symbol, interval=timeframe)
+        elif symbol is not None:
+            if date_from is not None and date_to is not None:
+                df = yf.download(symbol, start=date_from, end=date_to)
+            elif date_from is not None:
+                df = yf.download(symbol, start=date_from)
+            elif date_to is not None:
+                df = yf.download(symbol, end=date_to)
+            else:
+                df = yf.download(symbol)
+
+        if df.empty == False:
+            df.columns = ['Open', 'High', 'Low', 'Close', 'Volume']
+            return df.dropna()
